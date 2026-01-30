15f0d0c7152880ababda2c5ebcd5c7eab1660ab2
DESKTOP-OHLTK10\zane <ac2ount930@naver.com>
2026-01-30 14:50:34 +0900

    setting

---
+++ golden-harvest-sales/src/main/java/com/teamgold/goldenharvestsales/GoldenHarvestSalesApplication.java
@@ -0,0 +1,13 @@
+package com.teamgold.goldenharvestsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenHarvestSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenHarvestSalesApplication.class, args);
+	}
+
+}
---
+++ golden-harvest-sales/src/test/java/com/teamgold/goldenharvestsales/GoldenHarvestSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamgold.goldenharvestsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenHarvestSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
