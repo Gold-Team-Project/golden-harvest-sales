0d77fdc425469897f37695727d7e7461de102a55
jiwonchoi <jiwonchoi@jiwonchoiui-MacBookAir.local>
2026-02-01 18:25:54 +0900

    refactor: UserStatusUpdated event naming fix

--- golden-harvest-sales/src/main/java/com/teamgold/goldenharvestsales/common/config/KafkaProdConfig.java
+++ golden-harvest-sales/src/main/java/com/teamgold/goldenharvestsales/common/config/KafkaProdConfig.java
@@ -41,7 +41,7 @@ public class KafkaProdConfig {
         props.put("spring.json.use.type.headers", true);
 
         String typeMapping = "ItemMasterUpdatedEvent:com.teamgold.goldenharvestsales.sales.command.application.event.dto.ItemMasterUpdatedEvent, " +
-                "UserUpdatedEvent:com.teamgold.goldenharvestsales.sales.command.application.event.dto.UserUpdatedEvent";
+                "UserStatusUpdatedEvent:com.teamgold.goldenharvestsales.sales.command.application.event.dto.UserStatusUpdatedEvent";
         props.put("spring.json.type.mapping", typeMapping);
 
         props.put("spring.json.trusted.packages", "*");
