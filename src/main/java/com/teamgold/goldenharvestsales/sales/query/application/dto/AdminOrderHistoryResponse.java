881d925f1cc4d7b6abae525683c5d22aa1b3de5b
KyujinPark01 <rbwls1100@naver.com>
2026-02-09 23:37:14 +0900

    Fix: Change createdAt type to LocalDateTime in AdminOrderHistoryResponse

--- src/main/java/com/teamgold/goldenharvestsales/sales/query/application/dto/AdminOrderHistoryResponse.java
+++ src/main/java/com/teamgold/goldenharvestsales/sales/query/application/dto/AdminOrderHistoryResponse.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -16,7 +16,7 @@ import java.util.List;
 public class AdminOrderHistoryResponse {
     private String salesOrderId; // 주문번호
     private String orderStatus; // 상태
-    private LocalDate createdAt; // 생성일
+    private LocalDateTime createdAt; // 생성일
     private BigDecimal totalAmount; // 총 가격
     private String orderStatusType; // 상태 타입 (English)
     private String company; // 회사 이름
